daa168f9c28dc51a1897b76c00351592fdc94169
gusty <1261319+gusty@users.noreply.github.com>
2018-10-05 18:12:53 +0100

    Add suffix (build number)

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-dotnet pack src/Newtonsoft.Json.Converters.FSharp --configuration Release -o "%CD%\bin"
+dotnet pack src/Newtonsoft.Json.Converters.FSharp --configuration Release -o "%CD%\bin" --version-suffix CI%1
 if ERRORLEVEL 1 (echo Error building Newtonsoft.Json.Converters.FSharp; exit /b 1)
