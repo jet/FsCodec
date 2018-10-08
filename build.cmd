eac294ee3bc5fc2a258d81d40bf91a64241653a8
gusty <1261319+gusty@users.noreply.github.com>
2018-10-08 15:52:48 +0100

    Add tests to build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,5 @@
 dotnet pack src/Newtonsoft.Json.Converters.FSharp --configuration Release -o "%CD%\bin" --version-suffix CI%1
 if ERRORLEVEL 1 (echo Error building Newtonsoft.Json.Converters.FSharp; exit /b 1)
+
+dotnet test tests/Newtonsoft.Json.Converters.FSharp.Tests --configuration Release	
+if ERRORLEVEL 1 (echo Error testing Newtonsoft.Json.Converters.FSharp; exit /b 1)
