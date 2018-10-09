8c20c085de709a3ca4a14c31a3110976489c6269
Ruben Bartelink <ruben@bartelink.com>
2018-10-09 13:12:15 +0100

    Add JsonIsomorphismTests Add GuidConverter deemphasizing notice

--- tests/Jet.JsonNet.Converters.Tests/PicklerTests.fs
+++ tests/Jet.JsonNet.Converters.Tests/PicklerTests.fs
@@ -60,17 +60,41 @@ and private CartIdJsonConverter() =
     /// Input must be a Guid.Parseable value
     override __.UnPickle input = CartId.Parse input
 
-/// <summary>
-///     Renders all Guids without dashes.
-/// </summary>
-/// <remarks>
-///     Can work correctly as a global converter, as some codebases do for historical reasons
-///     Could arguably be usable as base class for various converters, including the above.
-///     However, the above pattern and variants thereof are recommended for new types.
-///     In general, the philosophy is that, beyond the Pickler base types, an identiy type should consist of explicit
-///       code as much as possible, and global converters really have to earn their keep - magic starts with -100 points.
-/// </remarks>
-type GuidConverter() =
-    inherit JsonIsomorphism<Guid, string>()
-    override __.Pickle g = g.ToString "N"
-    override __.UnPickle g = Guid.Parse g
+module JsonIsomorphismTests =
+
+    // NB Feel free to ignore this opinion and copy the 4 lines into your own globals - the pinning test will remain here
+    /// <summary>
+    ///     Renders all Guids without dashes.
+    /// </summary>
+    /// <remarks>
+    ///     Can work correctly as a global converter, as some codebases do for historical reasons
+    ///     Could arguably be usable as base class for various converters, including the above.
+    ///     However, the above pattern and variants thereof are recommended for new types.
+    ///     In general, the philosophy is that, beyond the Pickler base types, an identiy type should consist of explicit
+    ///       code as much as possible, and global converters really have to earn their keep - magic starts with -100 points.
+    /// </remarks>
+    type GuidConverter() =
+        inherit JsonIsomorphism<Guid, string>()
+        override __.Pickle g = g.ToString "N"
+        override __.UnPickle g = Guid.Parse g
+
+    type WithEmbeddedGuid = { a: string; [<JsonConverter(typeof<GuidConverter>)>] b: Guid }
+
+    let [<Fact>] ``Tagging with GuidConverter`` () =
+        let value = { a = "testing"; b = Guid.Empty }
+
+        let result = JsonConvert.SerializeObject value
+
+        test <@ """{"a":"testing","b":"00000000000000000000000000000000"}""" = result @>
+
+    let [<Fact>] ``Global GuidConverter`` () =
+        let settings = Settings.CreateDefault()
+        let value = Guid.Empty
+
+        let resDashes = JsonConvert.SerializeObject(value, settings)
+
+        settings.Converters.Add(new GuidConverter())
+        let resNoDashes = JsonConvert.SerializeObject(value, settings)
+
+        test <@ "\"00000000-0000-0000-0000-000000000000\"" = resDashes
+                && "\"00000000000000000000000000000000\"" = resNoDashes @>
