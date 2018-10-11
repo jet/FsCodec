4c5ab202a3eaa09321c44f305daee6ba7c894811
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2018-10-11 18:30:09 +0100

    Pad build numbers and prefix PRs
    This will create stuff like:
    `MyLib.0.1.0-CI0000798.nupkg` (from master)
    `MyLib.0.1.0-pr17-0000798.nupkg` (from PRs)

--- build.cmd
+++ build.cmd
@@ -1,5 +1,13 @@
-dotnet pack src/Jet.JsonNet.Converters --configuration Release -o "%CD%\bin" --version-suffix CI%1
+SETLOCAL
+SET X=%1
+SET BLD=000000%X%
+SET BLD=%BLD:~-7%
+if [%2]==[] (SET V=CI%BLD%) else (SET V=pr%2-%BLD%)
+  
+dotnet pack src/Jet.JsonNet.Converters --configuration Release -o "%CD%\bin" --version-suffix %V%
 if ERRORLEVEL 1 (echo Error building Jet.JsonNet.Converters; exit /b 1)
 
 dotnet test tests/Jet.JsonNet.Converters.Tests --configuration Release	
 if ERRORLEVEL 1 (echo Error testing Jet.JsonNet.Converters; exit /b 1)
+
+ENDLOCAL
