c11414e59e04efef45ba31d0caa24f919f11f5ee
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2018-10-11 18:30:09 +0100

    Pad build numbers; move logic to build.proj
    This will create stuff like:
    `MyLib.0.1.0-CI0000798.nupkg` (from master)
    `MyLib.0.1.0-CI0000798-pr17.nupkg` (from PRs)

--- build.cmd
+++ build.cmd
@@ -1,5 +1,2 @@
-dotnet pack src/Jet.JsonNet.Converters --configuration Release -o "%CD%\bin" --version-suffix CI%1
-if ERRORLEVEL 1 (echo Error building Jet.JsonNet.Converters; exit /b 1)
-
-dotnet test tests/Jet.JsonNet.Converters.Tests --configuration Release	
-if ERRORLEVEL 1 (echo Error testing Jet.JsonNet.Converters; exit /b 1)
+dotnet pack build.proj /p:BN=%1 /p:PR=%2
+dotnet test build.proj -v n
