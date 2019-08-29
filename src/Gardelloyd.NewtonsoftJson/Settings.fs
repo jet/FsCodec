3c54f3d7e4498445c26463e18dbbf7e91221671d
Ruben Bartelink <ruben@bartelink.com>
2019-08-29 23:00:29 +0100

    Split out codec impls

---
+++ src/Gardelloyd.NewtonsoftJson/Settings.fs
@@ -0,0 +1,60 @@
+namespace Gardelloyd.NewtonsoftJson
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
+open System
+open System.Runtime.InteropServices
+
+type Settings private () =
+
+    static let defaultConverters : JsonConverter[] = [| OptionConverter() |]
+
+    /// Creates a default set of serializer settings used by Json serialization. When used with no args, same as JsonSerializerSettings.CreateDefault()
+    static member CreateDefault
+        (   [<Optional;ParamArray>]converters : JsonConverter[],
+            /// Use multi-line, indented formatting when serializing json; defaults to false.
+            [<Optional;DefaultParameterValue(null)>]?indent : bool,
+            /// Render idiomatic camelCase for PascalCase items by using `CamelCasePropertyNamesContractResolver`. Defaults to false.
+            [<Optional;DefaultParameterValue(null)>]?camelCase : bool,
+            /// Ignore null values in input data; defaults to false.
+            [<Optional;DefaultParameterValue(null)>]?ignoreNulls : bool,
+            /// Error on missing values (as opposed to letting them just be default-initialized); defaults to false.
+            [<Optional;DefaultParameterValue(null)>]?errorOnMissing : bool) =
+        let indent = defaultArg indent false
+        let camelCase = defaultArg camelCase false
+        let ignoreNulls = defaultArg ignoreNulls false
+        let errorOnMissing = defaultArg errorOnMissing false
+        let resolver : IContractResolver =
+             if camelCase then CamelCasePropertyNamesContractResolver() :> _
+             else DefaultContractResolver() :> _
+        JsonSerializerSettings(
+            ContractResolver = resolver,
+            Converters = converters,
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc, // Override default of RoundtripKind
+            DateFormatHandling = DateFormatHandling.IsoDateFormat, // Pin Json.Net claimed default
+            Formatting = (if indent then Formatting.Indented else Formatting.None),
+            MissingMemberHandling = (if errorOnMissing then MissingMemberHandling.Error else MissingMemberHandling.Ignore),
+            NullValueHandling = (if ignoreNulls then NullValueHandling.Ignore else NullValueHandling.Include))
+
+    /// Optionated helper that creates serializer settings that provide good defaults for F#
+    /// - no camel case conversion - assumption is you'll use records with camelCased names
+    /// - Always prepends an OptionConverter() to any converters supplied
+    /// - everything else is as per CreateDefault:- i.e. emit nulls instead of omitting fields etc
+    static member Create
+        (   /// List of converters to apply. An implicit OptionConverter() will be prepended and/or be used as a default
+            [<Optional;ParamArray>]converters : JsonConverter[],
+            /// Use multi-line, indented formatting when serializing json; defaults to false.
+            [<Optional;DefaultParameterValue(null)>]?indent : bool,
+            /// Render idiomatic camelCase for PascalCase items by using `CamelCasePropertyNamesContractResolver`.
+            ///  Defaults to false on basis that you'll use record and tuple field names that are camelCase (and hence not `CLSCompliant`).
+            [<Optional;DefaultParameterValue(null)>]?camelCase : bool,
+            /// Ignore null values in input data; defaults to `false`.
+            [<Optional;DefaultParameterValue(null)>]?ignoreNulls : bool,
+            /// Error on missing values (as opposed to letting them just be default-initialized); defaults to false
+            [<Optional;DefaultParameterValue(null)>]?errorOnMissing : bool) =
+        Settings.CreateDefault(
+            converters=(match converters with null | [||] -> defaultConverters | xs -> Array.append defaultConverters xs),
+            ?ignoreNulls=ignoreNulls,
+            ?errorOnMissing=errorOnMissing,
+            ?indent=indent,
+            ?camelCase=camelCase)
---
+++ src/Gardelloyd/Codec.fs
@@ -0,0 +1,35 @@
+namespace Gardelloyd
+
+/// Provides Codecs that render to a UTF-8 array suitable for storage in EventStore or CosmosDb based on explicit functions you supply
+/// i.e., with using conventions / Type Shapes / Reflection or specific Json processing libraries - see Gardelloyd.*.Codec for batteries-included Coding/Decoding
+type Codec =
+
+    /// Generate a codec suitable for use with <c>Equinox.EventStore</c>, <c>Equinox.Cosmos</c> or <c>Propulsion</c> libraries
+    /// using the supplied pair of <c>encode</c> and <c>tryDecode</code> functions.
+    // Leaving this private until someone actually asks for this (IME, while many systems have some code touching the metadata, it tends to fall into disuse)
+    static member private Create<'Union>
+        (   /// Maps a 'Union to an Event Type Name with UTF-8 arrays representing the `Data` and `Metadata`.
+            encode : 'Union -> string * byte[] * byte[],
+            /// Attempts to map from an Event Type Name and UTF-8 arrays representing the `Data` and `Metadata` to a 'Union case, or None if not mappable.
+            tryDecode : string * byte[] * byte[] -> 'Union option
+                                        )
+        : IUnionEncoder<'Union,byte[]> =
+        { new IUnionEncoder<'Union, byte[]> with
+            member __.Encode e =
+                let eventType, payload, metadata = encode e
+                Core.EventData.Create(eventType,payload,metadata,?timestamp=None) :> _
+            member __.TryDecode ee =
+                tryDecode (ee.EventType, ee.Data, ee.Meta) }
+
+    /// Generate a codec suitable for use with <c>Equinox.EventStore</c>, <c>Equinox.Cosmos</c> or <c>Propulsion</c> libraries,
+    /// using the supplied pair of <c>encode</c> and <c>tryDecode</code> functions.
+    static member Create<'Union>
+        (   /// Maps a 'Union to an Event Type Name and a UTF-8 array representing the `Data`.
+            encode : 'Union -> string * byte[],
+            /// Attempts to map an Event Type Name and a UTF-8 `Data` array to a 'Union case, or None if not mappable.
+            tryDecode : string * byte[] -> 'Union option
+                                )
+        : IUnionEncoder<'Union,byte[]> =
+        let encode' value = let c, d = encode value in c, d, null
+        let tryDecode' (et,d,_md) = tryDecode (et, d)
+        Codec.Create(encode', tryDecode')
