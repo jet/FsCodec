7825472155efec8f537871c2711fd99b43de6a6f
Ruben Bartelink <ruben@bartelink.com>
2020-02-17 09:26:14 +0000

    Fix matching

--- tests/FsCodec.NewtonsoftJson.Tests/StreamNameTests.fs
+++ tests/FsCodec.NewtonsoftJson.Tests/StreamNameTests.fs
@@ -11,11 +11,11 @@ let [<Fact>] ``Can roundtrip multi-ids with embedded dashes`` () =
 
     test <@ StreamName.parse "Cat-a-b_c-d" = sn @>
 
-    test <@ let (StreamName.CategoryAndIds ("Cat", elems)) = sn
-            [e1;e2] = List.ofArray elems @>
+    test <@ let (StreamName.CategoryAndIds (scat, elems)) = sn
+            scat = cat && [e1;e2] = List.ofArray elems @>
 
-    test <@ let (StreamName.CategoryAndId ("Cat", aggId)) = sn
-            aggId = "a-b_c-d" @>
+    test <@ let (StreamName.CategoryAndId (scat, aggId)) = sn
+            scat = cat && aggId = "a-b_c-d" @>
 
 let [<Fact>] ``Can roundtrip single aggregateIs with embedded dashes and underscores`` () =
     let cat, aggId = "Cat", "a-b_c-d"
@@ -24,8 +24,7 @@ let [<Fact>] ``Can roundtrip single aggregateIs with embedded dashes and undersc
 
     test <@ StreamName.parse "Cat-a-b_c-d" = sn @>
 
-    test <@ let (StreamName.CategoryAndId ("Cat", aggId)) = sn
-            "a-b_c-d" = aggId @>
-    test <@ let (StreamName.CategoryAndIds ("Cat", aggIds)) = sn
-            ["a-b";"c-d"] = List.ofArray aggIds @>
-
+    test <@ let (StreamName.CategoryAndId (scat, aggId)) = sn
+            scat = cat && "a-b_c-d" = aggId @>
+    test <@ let (StreamName.CategoryAndIds (scat, aggIds)) = sn
+            scat = cat && ["a-b";"c-d"] = List.ofArray aggIds @>
