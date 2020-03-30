1ac36debb3d9cc8812afddeef16c28678b9cb8c2
Nick Darvey <nickdarvey@outlook.com.au>
2020-03-31 00:43:26 +1100

    Makes things compile * Replaces Newtonsoft bits from scaffold with the actual STJ bits * Tries to retain symmetry with Newtonsoft UnionConverter impl

--- src/FsCodec.SystemTextJson/JsonSerializerElementExtensions.fs
+++ src/FsCodec.SystemTextJson/JsonSerializerElementExtensions.fs
@@ -7,6 +7,16 @@ open System.Text.Json
 
 [<AutoOpen>]
 module internal JsonSerializerExtensions =
+#if NETSTANDARD2_0
+    let private write (element : JsonElement )=
+        let bufferWriter = ArrayBufferWriter<byte>()
+        (
+            use jsonWriter = new Utf8JsonWriter(bufferWriter)
+            element.WriteTo(jsonWriter)
+        )
+        bufferWriter.WrittenSpan
+#endif
+
     type JsonSerializer with
         static member SerializeToElement(value: 'T, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
             let span = ReadOnlySpan.op_Implicit(JsonSerializer.SerializeToUtf8Bytes(value, defaultArg options null))
@@ -14,13 +24,14 @@ module internal JsonSerializerExtensions =
 
         static member DeserializeElement<'T>(element: JsonElement, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
 #if NETSTANDARD2_0
-            let json = element.GetRawText()
-            JsonSerializer.Deserialize<'T>(json, defaultArg options null)
+            JsonSerializer.Deserialize<'T>(write element, defaultArg options null)
 #else
-            let bufferWriter = ArrayBufferWriter<byte>()
-            (
-                use jsonWriter = new Utf8JsonWriter(bufferWriter)
-                element.WriteTo(jsonWriter)
-            )
-            JsonSerializer.Deserialize<'T>(bufferWriter.WrittenSpan, defaultArg options null)
+            JsonSerializer.Deserialize<'T>(element.GetRawText(), defaultArg options null)
+#endif
+
+        static member DeserializeElement(element : JsonElement, t : Type, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
+#if NETSTANDARD2_0
+            JsonSerializer.Deserialize(write element, t, defaultArg options null)
+#else
+            JsonSerializer.Deserialize(element.GetRawText(), t, defaultArg options null)
 #endif
--- src/FsCodec.SystemTextJson/UnionConverter.fs
+++ src/FsCodec.SystemTextJson/UnionConverter.fs
@@ -5,6 +5,8 @@ open System
 open System.Reflection
 open System.Text.Json
 
+open FsCodec.SystemTextJson.Core
+
 [<NoComparison; NoEquality>]
 type private Union =
     {
@@ -46,24 +48,26 @@ module private Union =
 
     /// Prepare arguments for the Case class ctor based on the kind of case and how F# maps that to a Type
     /// and/or whether we need to let json.net step in to convert argument types
-    let mapTargetCaseArgs (inputJObject : JsonDocument) serializer : PropertyInfo[] -> obj [] = function
+    let mapTargetCaseArgs (element : JsonElement) options : PropertyInfo[] -> obj [] = function
         | [| singleCaseArg |] when propTypeRequiresConstruction singleCaseArg.PropertyType ->
-            [| inputJObject.(singleCaseArg.PropertyType, serializer) |]
+            [| JsonSerializer.DeserializeElement (element, singleCaseArg.PropertyType, options) |]
         | multipleFieldsInCustomCaseType ->
             [| for fi in multipleFieldsInCustomCaseType ->
-                match inputJObject.[fi.Name] with
-                | null when
-                    // Afford converters an opportunity to handle the missing field in the best way I can figure out to signal that
-                    // The specific need being covered (see tests) is to ensure that, even with MissingMemberHandling=Ignore,
-                    // the TypeSafeEnumConverter should reject missing values
-                    // not having this case would go direct to `null` without passing go
-                    typeHasJsonConverterAttribute fi.PropertyType
-                    || serializer.MissingMemberHandling = MissingMemberHandling.Error ->
-                        // NB caller can opt out of erroring by setting NullValueHandling = NullValueHandling.Ignore)
-                        // which renders the following equivalent to the next case
-                        JToken.Parse("null").ToObject(fi.PropertyType, serializer)
-                | null -> null
-                | itemValue -> itemValue.ToObject(fi.PropertyType, serializer) |]
+                match element.GetProperty fi.Name with
+                // TOTHINK: I'm not sure this applies with STJ
+
+                //| null when
+                //    // Afford converters an opportunity to handle the missing field in the best way I can figure out to signal that
+                //    // The specific need being covered (see tests) is to ensure that, even with MissingMemberHandling=Ignore,
+                //    // the TypeSafeEnumConverter should reject missing values
+                //    // not having this case would go direct to `null` without passing go
+                //    typeHasJsonConverterAttribute fi.PropertyType
+                //    || serializer.MissingMemberHandling = MissingMemberHandling.Error ->
+                //        // NB caller can opt out of erroring by setting NullValueHandling = NullValueHandling.Ignore)
+                //        // which renders the following equivalent to the next case
+                //        JToken.Parse("null").ToObject(fi.PropertyType, serializer)
+                | el when el.ValueKind = JsonValueKind.Null -> null
+                | el -> JsonSerializer.DeserializeElement (el, fi.PropertyType, options) |]
 
 /// Serializes a discriminated union case with a single field that is a
 /// record by flattening the record fields to the same level as the discriminator
@@ -76,7 +80,8 @@ type UnionConverter<'T> private (discriminator : string, ?catchAllCase) =
     override __.CanConvert (t : Type) = Union.tryGetUnion t |> Option.isSome
 
     override __.Write(writer, value, options) =
-        let union = Union.tryGetUnion (value.GetType())
+        let value = box value
+        let union = Union.tryGetUnion (value.GetType()) |> Option.get // TOASK: Do we wanna keep the try?
         let tag = union.tagReader value
         let case = union.cases.[tag]
         let fieldValues = union.fieldReader.[tag] value
@@ -85,43 +90,43 @@ type UnionConverter<'T> private (discriminator : string, ?catchAllCase) =
         writer.WriteStartObject()
 
         writer.WritePropertyName(discriminator)
-        writer.WriteValue(case.Name)
+        writer.WriteStringValue(case.Name)
 
         match fieldInfos with
         | [| fi |] ->
             match fieldValues.[0] with
-            | null when serializer.NullValueHandling = NullValueHandling.Ignore -> ()
+            | null when options.IgnoreNullValues -> ()
             | fv ->
-                let token = if fv = null then JToken.Parse "null" else JToken.FromObject(fv, serializer)
-                match token.Type with
-                | JTokenType.Object ->
+                let element = JsonSerializer.SerializeToElement(fv, options)
+                match element.ValueKind with
+                | JsonValueKind.Object ->
                     // flatten the object properties into the same one as the discriminator
-                    for prop in token.Children() do
+                    for prop in element.EnumerateObject() do
                         prop.WriteTo writer
                 | _ ->
                     writer.WritePropertyName(fi.Name)
-                    token.WriteTo writer
+                    element.WriteTo writer
         | _ ->
             for fieldInfo, fieldValue in Seq.zip fieldInfos fieldValues do
-                if fieldValue <> null || serializer.NullValueHandling = NullValueHandling.Include then
+                if fieldValue <> null || not options.IgnoreNullValues then
                     writer.WritePropertyName(fieldInfo.Name)
-                    serializer.Serialize(writer, fieldValue)
+                    JsonSerializer.Serialize(writer, fieldValue, options)
 
         writer.WriteEndObject()
 
-    override __.Read(reader, t : Type, options) =
-        let token = JToken.ReadFrom reader
-        if token.Type <> JTokenType.Object then raise (FormatException(sprintf "Expected object token, got %O" token.Type))
-        let inputJObject = token :?> JObject
+    override __.Read(reader, t : Type, options) =   // TOASK: Change order of methods to satisfy non-uniform instantiation warning
+        reader.ValidateTokenType(JsonTokenType.StartObject)
+        use document = JsonDocument.ParseValue &reader
+        let element = document.RootElement
 
-        let union = Union.getUnion t
+        let union = Union.tryGetUnion t |> Option.get // TOASK: Do we wanna keep the try?
         let targetCaseIndex =
-            let inputCaseNameValue = inputJObject.[discriminator] |> string
+            let inputCaseNameValue = element.GetProperty discriminator |> string
             let findCaseNamed x = union.cases |> Array.tryFindIndex (fun case -> case.Name = x)
             match findCaseNamed inputCaseNameValue, catchAllCase  with
             | None, None ->
                 sprintf "No case defined for '%s', and no catchAllCase nominated for '%s' on type '%s'"
-                    inputCaseNameValue typeof<UnionConverter>.Name t.FullName |> invalidOp
+                    inputCaseNameValue typeof<UnionConverter<_>>.Name t.FullName |> invalidOp
             | Some foundIndex, _ -> foundIndex
             | None, Some catchAllCaseName ->
                 match findCaseNamed catchAllCaseName with
@@ -131,4 +136,4 @@ type UnionConverter<'T> private (discriminator : string, ?catchAllCase) =
                 | Some foundIndex -> foundIndex
 
         let targetCaseFields, targetCaseCtor = union.cases.[targetCaseIndex].GetFields(), union.caseConstructor.[targetCaseIndex]
-        targetCaseCtor (Union.mapTargetCaseArgs inputJObject serializer targetCaseFields)
+        targetCaseCtor (Union.mapTargetCaseArgs element options targetCaseFields) :?> 'T
