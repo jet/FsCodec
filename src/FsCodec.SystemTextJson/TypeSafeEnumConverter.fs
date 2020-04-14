b4cf917fe1d806a5141889fd173e43c1ab0b15bf
Ruben Bartelink <ruben@bartelink.com>
2020-04-14 18:23:13 +0100

    Generalize FsCodec.SystemTextJson.TypeSafeEnum.tryParseT

--- src/FsCodec.SystemTextJson/TypeSafeEnumConverter.fs
+++ src/FsCodec.SystemTextJson/TypeSafeEnumConverter.fs
@@ -10,29 +10,29 @@ module TypeSafeEnum =
     let private _isTypeSafeEnum (t : Type) =
         Union.tryGetUnion t
         |> Option.exists (fun u -> u.cases |> Seq.forall (fun case -> case.GetFields().Length = 0))
-    let isTypeSafeEnum = memoize _isTypeSafeEnum
+    let isTypeSafeEnum : Type -> bool = memoize _isTypeSafeEnum
 
-    let tryParseT (t : Type) (str : string) =
+    let tryParseT (t : Type) predicate =
         match Union.tryGetUnion t with
-        | None -> invalidArg "t" "Type must be a FSharpUnion."
+        | None   -> invalidArg "t" "Type must be a FSharpUnion."
         | Some u ->
             u.cases
-            |> Array.tryFindIndex (fun case -> case.Name = str)
+            |> Array.tryFindIndex (fun c -> predicate c.Name)
             |> Option.map (fun tag -> u.caseConstructor.[tag] [||])
             // TOCONSIDER memoize and/or push into `Union` https://github.com/jet/FsCodec/pull/41#discussion_r394473137
-    let tryParse<'T> (str : string) = tryParseT typeof<'T> str |> Option.map (fun e -> e :?> 'T)
+    let tryParse<'T> (str : string) = tryParseT typeof<'T> ((=) str) |> Option.map (fun e -> e :?> 'T)
 
-    let parseT (t : Type) (str : string) =
-        match tryParseT t str with
+    let parseT (t : Type) (str : string)  =
+        match tryParseT t ((=) str) with
         | Some e -> e
         | None   ->
             // Keep exception compat, but augment with a meaningful message.
             raise (KeyNotFoundException(sprintf "Could not find case '%s' for type '%s'" str t.FullName))
     let parse<'T> (str : string) = parseT typeof<'T> str :?> 'T
 
-    let toString (x : obj) =
-        let union = x.GetType() |> Union.tryGetUnion |> Option.get
-        let tag = union.tagReader x
+    let toString<'t> (x : 't) =
+        let union = Union.tryGetUnion (typeof<'t>) |> Option.get
+        let tag = union.tagReader (box x)
         // TOCONSIDER memoize and/or push into `Union` https://github.com/jet/FsCodec/pull/41#discussion_r394473137
         union.cases.[tag].Name
 
@@ -41,7 +41,7 @@ type TypeSafeEnumConverter<'T>() =
     inherit Serialization.JsonConverter<'T>()
 
     override __.CanConvert(t : Type) =
-        TypeSafeEnum.isTypeSafeEnum t
+        t = typedefof<'T> && TypeSafeEnum.isTypeSafeEnum typedefof<'T>
 
     override __.Write(writer, value, _options) =
         let str = TypeSafeEnum.toString value
