3afb011b8ecedfaccc4bc4213ed87ead0f15abc7
Ruben Bartelink <ruben@bartelink.com>
2022-01-03 10:50:34 +0000

    Tidy

--- src/FsCodec.SystemTextJson/JsonSerializerElementExtensions.fs
+++ src/FsCodec.SystemTextJson/JsonSerializerElementExtensions.fs
@@ -9,26 +9,13 @@ open System.Text.Json
 module internal JsonSerializerExtensions =
 
     type JsonSerializer with
-        static member SerializeToElement(value: 'T, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
-            let span = ReadOnlySpan.op_Implicit(JsonSerializer.SerializeToUtf8Bytes(value, defaultArg options null))
-            JsonSerializer.Deserialize<JsonElement>(span)
-
         static member DeserializeElement<'T>(element: JsonElement, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
-#if NETSTANDARD2_0
-            let json = element.GetRawText()
-            JsonSerializer.Deserialize<'T>(json, defaultArg options null)
-#else
             let bufferWriter = ArrayBufferWriter<byte>()
             (
                 use jsonWriter = new Utf8JsonWriter(bufferWriter)
                 element.WriteTo(jsonWriter)
             )
             JsonSerializer.Deserialize<'T>(bufferWriter.WrittenSpan, defaultArg options null)
-#endif
 
         static member DeserializeElement(element : JsonElement, t : Type, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
-#if NETSTANDARD2_0
-            JsonSerializer.Deserialize(write element, t, defaultArg options null)
-#else
             JsonSerializer.Deserialize(element.GetRawText(), t, defaultArg options null)
-#endif
