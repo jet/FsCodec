6234435f6d551ab3a40a18fe2ccc2cc2029f78d1
Ruben Bartelink <ruben@bartelink.com>
2022-01-03 19:41:20 +0000

    Revert accidental change

--- src/FsCodec.NewtonsoftJson/Settings.fs
+++ src/FsCodec.NewtonsoftJson/Settings.fs
@@ -7,6 +7,8 @@ open System.Runtime.InteropServices
 
 type Settings private () =
 
+    static let defaultConverters : JsonConverter[] = [| OptionConverter() |]
+
     /// Creates a default set of serializer settings used by Json serialization. When used with no args, same as JsonSerializerSettings.CreateDefault()
     static member CreateDefault
         (   [<Optional; ParamArray>] converters : JsonConverter[],
@@ -54,7 +56,7 @@ type Settings private () =
             [<Optional; DefaultParameterValue(null)>] ?errorOnMissing : bool) =
 
         Settings.CreateDefault(
-            converters = converters,
+            converters = (match converters with null | [||] -> defaultConverters | xs -> Array.append defaultConverters xs),
             ?ignoreNulls = ignoreNulls,
             ?errorOnMissing = errorOnMissing,
             ?indent = indent,
