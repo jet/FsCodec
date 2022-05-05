a27c172e0179b53b2a1a5ed398d767b49552d84e
Ruben Bartelink <ruben@bartelink.com>
2022-05-05 13:06:23 +0100

    Remove obsolete code

--- src/FsCodec.SystemTextJson/Serdes.fs
+++ src/FsCodec.SystemTextJson/Serdes.fs
@@ -1,6 +1,5 @@
 namespace FsCodec.SystemTextJson
 
-open System.Runtime.InteropServices
 open System.Text.Json
 
 /// Serializes to/from strings using the supplied Options
@@ -16,32 +15,3 @@ type Serdes(options : JsonSerializerOptions) =
     /// Deserializes value of given type from JSON string.
     member x.Deserialize<'T>(json : string) : 'T =
         JsonSerializer.Deserialize<'T>(json, options)
-
-    /// Serializes given value to a JSON string.
-    [<System.Obsolete "Please use non-static Serdes instead">]
-    static member Serialize<'T>
-        (   /// Value to serialize.
-            value : 'T,
-            /// Use indentation when serializing JSON. Defaults to false.
-            [<Optional; DefaultParameterValue false>] ?indent : bool) : string =
-        let options = (if indent = Some true then Options.Create(indent = true) else Options.Default)
-        JsonSerializer.Serialize<'T>(value, options)
-
-    /// Serializes given value to a JSON string with custom options
-    [<System.Obsolete "Please use non-static Serdes instead">]
-    static member Serialize<'T>
-        (   /// Value to serialize.
-            value : 'T,
-            /// Options to use (use other overload to use Options.Default profile)
-            options : JsonSerializerOptions) : string =
-        JsonSerializer.Serialize<'T>(value, options)
-
-    /// Deserializes value of given type from JSON string.
-    [<System.Obsolete "Please use non-static Serdes instead">]
-    static member Deserialize<'T>
-        (   /// Json string to deserialize.
-            json : string,
-            /// Options to use (defaults to Options.Default profile)
-            [<Optional; DefaultParameterValue null>] ?options : JsonSerializerOptions) : 'T =
-        let settings = match options with Some o -> o | None -> Options.Default
-        JsonSerializer.Deserialize<'T>(json, settings)
