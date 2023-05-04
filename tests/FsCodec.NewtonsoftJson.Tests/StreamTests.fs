6f0016cddced9da42dab1c726117efa038472423
Asti <rahul.ramakumar@walmart.com>
2023-05-04 16:40:18 +0530

    Restore 'classic' test

--- tests/FsCodec.NewtonsoftJson.Tests/StreamTests.fs
+++ tests/FsCodec.NewtonsoftJson.Tests/StreamTests.fs
@@ -9,20 +9,13 @@ open FsCodec.NewtonsoftJson
 open Swensen.Unquote
 open System.IO
 open Xunit
+let serdes = Serdes Options.Default
 
 type Rec = { a : int; b : string; c : string }
-
 let [<Fact>] ``Can serialize/deserialize to stream`` () =
     let value = { a = 10; b = "10"; c = "" }
     use stream = new MemoryStream()
-    Serdes.Default.SerializeToStream(value, stream)
-#if SYSTEM_TEXT_JSON
-    stream.Length <>! 0 // TODO @deviousasti, why not for JSON.NET
-#endif
-    stream.Seek(0L, SeekOrigin.Begin) =! 0
-    let value' = Serdes.Default.DeserializeFromStream<Rec>(stream)
-#if SYSTEM_TEXT_JSON
-    value =! value'
-#else
-    Assert.Null(value') // TODO @deviousasti, why?
-#endif
+    serdes.SerializeToStream(value, stream)
+    stream.Seek(0L, SeekOrigin.Begin) |> ignore
+    let deserialized = serdes.DeserializeFromStream(stream)
+    <@ value = deserialized @>
