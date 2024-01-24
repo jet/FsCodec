102decc1732591bf2e9bed6c8d3ca12d8538280e
Ruben Bartelink <ruben@bartelink.com>
2024-01-24 08:48:59 +0000

    Tidy

--- src/FsCodec.SystemTextJson/UnionOrTypeSafeEnumConverterFactory.fs
+++ src/FsCodec.SystemTextJson/UnionOrTypeSafeEnumConverterFactory.fs
@@ -1,18 +1,14 @@
 namespace FsCodec.SystemTextJson
 
-open System
-open System.Linq.Expressions
 open System.Text.Json.Serialization
-
-type internal ConverterActivator = delegate of unit -> JsonConverter
 
 type UnionOrTypeSafeEnumConverterFactory(typeSafeEnum, union) =
     inherit JsonConverterFactory()
 
-    static let hasConverterAttribute = memoize (fun (t: Type) -> t.IsDefined(typeof<JsonConverterAttribute>, true))
+    static let hasConverterAttribute = memoize (fun (t: System.Type) -> t.IsDefined(typeof<JsonConverterAttribute>, true))
 
-    override _.CanConvert(t: Type) =
-        not (t.IsGenericType && let gtd = t.GetGenericTypeDefinition() in gtd = typedefof<option<_>> || gtd = typedefof<list<_>>)
+    override _.CanConvert t =
+        not (t.IsGenericType && let g = t.GetGenericTypeDefinition() in g = typedefof<option<_>> || g = typedefof<list<_>>)
         && FsCodec.Union.isUnion t
         && not (hasConverterAttribute t)
         && ((typeSafeEnum && union)
@@ -20,9 +16,4 @@ type UnionOrTypeSafeEnumConverterFactory(typeSafeEnum, union) =
 
     override _.CreateConverter(t, _options) =
         let openConverterType = if FsCodec.Union.isNullary t then typedefof<TypeSafeEnumConverter<_>> else typedefof<UnionConverter<_>>
-        let constructor = openConverterType.MakeGenericType(t).GetConstructors() |> Array.head
-        let newExpression = Expression.New(constructor)
-        let lambda = Expression.Lambda(typeof<ConverterActivator>, newExpression)
-
-        let activator = lambda.Compile() :?> ConverterActivator
-        activator.Invoke()
+        openConverterType.MakeGenericType(t).GetConstructors().[0].Invoke[||] :?> _
